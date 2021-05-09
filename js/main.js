dd3b36b06795d7ebc9b97e42a168d1bbcd4bf510
Abdallah-Rabeh <abdallahaphd@gmail.com>
2021-05-09 07:59:35 +0200

    var

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$("svg").mouseenter(function () {
+    $(this).css('fill', '#1da1f2');
+});
+$("svg").mouseleave(function () {
+    $(this).css('fill', '#607d8b');
+});
--- js/main.ts
+++ js/main.ts
@@ -1,4 +1,9 @@
 
-
+$("svg").mouseenter(function(){
+    $(this).css('fill' , '#1da1f2')
+});
+$("svg").mouseleave(function(){
+    $(this).css('fill' , '#607d8b')
+});
 
 
